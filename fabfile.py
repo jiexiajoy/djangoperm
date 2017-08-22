1d760adaf5a950f69cf4cda4c951b790fefa791e
李满满 <xmdevops@vip.qq.com>
2017-08-22 13:04:49 +0800

    djangoperm

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 #! -*- coding: utf-8 -*-
 
 from __future__ import with_statement
+from itertools import dropwhile
 from fabric.api import *
 from fabric.colors import *
 from fabric.contrib.console import *
@@ -13,6 +14,7 @@ env.local_src_dir = '/Users/manmanli/xm-webs/djangoperm/'
 def prepare_deploy():
     with lcd(env.local_src_dir):
         with warn_only():
-            result = local('git add -A', capture=True).succeeded and local('git commit -m "djangoperm"', capture=True).succeeded and local('git push', capture=True).succeeded
-            if result:
-                abort(red(output.stderr))
+            commands = ['git add -A', 'git commit -m "djangoperm"', 'git push']
+            for res in dropwhile(lambda command: local(command).succeeded, commands):
+                abort(red(res))
+        print green('-- prepare_deploy success.')
